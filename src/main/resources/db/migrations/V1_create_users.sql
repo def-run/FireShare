e701d5a0f691f634fd5d21c276b4ea0a2d53a932
Def <def17062026@gmail.com>
2026-09-12 22:13:26 +0530

    created users db

---
+++ src/main/resources/db/migrations/V1_create_users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL
+)
