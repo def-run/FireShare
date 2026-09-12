d8f53aaa949889a2bd7e11a41ef80caf461398fb
Def <def17062026@gmail.com>
2026-09-12 22:11:47 +0530

    created file repository layer

--- src/main/java/org/def/fireshare/repository/FileRepository.java
+++ src/main/java/org/def/fireshare/repository/FileRepository.java
@@ -1,4 +1,7 @@
 package org.def.fireshare.repository;
 
-public interface FileRepository {
+import org.def.fireshare.model.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileRepository extends JpaRepository<File, Long> {
 }
