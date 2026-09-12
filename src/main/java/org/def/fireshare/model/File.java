3b61d63123fee5c25d257b9eb9ccff4709638bec
Def <def17062026@gmail.com>
2026-09-12 22:20:36 +0530

    generated getters and setters

--- src/main/java/org/def/fireshare/model/File.java
+++ src/main/java/org/def/fireshare/model/File.java
@@ -26,4 +26,52 @@ public class File {
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getStoragePath() {
+        return storagePath;
+    }
+
+    public void setStoragePath(String storagePath) {
+        this.storagePath = storagePath;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
---
+++ src/main/java/org/def/fireshare/service/FileService.java
@@ -0,0 +1,4 @@
+package org.def.fireshare.service;
+
+public class FileService {
+}
