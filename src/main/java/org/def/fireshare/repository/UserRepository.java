a01f7815f4fdfd17b3e7fcb7fe844e4991b3351f
Def <def17062026@gmail.com>
2026-09-12 22:12:15 +0530

    created user repository layer

--- src/main/java/org/def/fireshare/repository/UserRepository.java
+++ src/main/java/org/def/fireshare/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package org.def.fireshare.repository;
 
-public interface UserRepository {
+import org.def.fireshare.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
